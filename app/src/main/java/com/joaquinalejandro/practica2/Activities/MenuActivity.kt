66ed27aeda7f8b397ddce8e05a8ec63421a7d66a
alexi <alejandro.sanchezi@estudiante.uam.es>
2019-03-15 16:04:12 +0100

    funciona todo

---
+++ app/src/main/java/com/joaquinalejandro/practica2/Activities/MenuActivity.kt
@@ -0,0 +1,13 @@
+package com.joaquinalejandro.practica2.Activities
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.joaquinalejandro.practica2.R
+
+class MenuActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu)
+    }
+}
