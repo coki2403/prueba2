cb9c921919ff9e5a7de48c6a8bc8cbfe9235262f
coki2403 <joaquin.verdejo@outlook.es>
2019-03-17 11:52:46 +0100

    menu y logo

--- app/src/main/java/com/joaquinalejandro/practica2/Activities/MenuActivity.kt
+++ app/src/main/java/com/joaquinalejandro/practica2/Activities/MenuActivity.kt
@@ -1,13 +1,20 @@
 package com.joaquinalejandro.practica2.Activities
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.joaquinalejandro.practica2.R
+import kotlinx.android.synthetic.main.activity_menu.*
+import java.lang.System.exit
 
 class MenuActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu)
+
+        nuevapartida.setOnClickListener({startActivity(Intent(this@MenuActivity, MainActivity::class.java))})
+        cargarpartida.setOnClickListener({startActivity(Intent(this@MenuActivity, PartidaListaActivity::class.java))})
+        salir.setOnClickListener({exit(0)})
     }
 }
